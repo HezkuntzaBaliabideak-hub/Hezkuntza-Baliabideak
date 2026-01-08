bb3c37c1242e10b583f1336babc39dce9776bdf6
HezkuntzaBaliabideak-hub <naiaramungia.157@gmail.com>
2026-01-08 12:48:52 +0100

    Subida inicial de la web del TFG

---
+++ web-formulario/src/main/java/com/formulario/WebFormularioApplication.java
@@ -0,0 +1,13 @@
+package com.formulario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFormularioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFormularioApplication.class, args);
+	}
+
+}
